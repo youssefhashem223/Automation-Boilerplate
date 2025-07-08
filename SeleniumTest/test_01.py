c5537ef9722c8a1c5b4f2be92b946bb1e4cb371e
FlamingoLindo <101421364+FlamingoLindo@users.noreply.github.com>
2025-07-08 10:53:16 -0300

    feat: add Appium and Selenium test files for initial testing setup

---
+++ SeleniumTest/test_01.py
@@ -0,0 +1,32 @@
+"""
+Selenium Test 01
+"""
+
+import os
+import sys
+from dotenv import load_dotenv
+
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+
+load_dotenv()
+
+path_to_add = os.path.abspath(os.path.join(
+    os.path.dirname(__file__), '..', 'PyUtils'))
+sys.path.append(path_to_add)
+
+
+def test_01(driver):
+    """
+    Test 01.
+    """
+    driver, wait = driver
+
+    test = wait.until(EC.element_to_be_clickable((By.ID, 'test'))).click()
+    assert test.is_displayed(), "Test element is not displayed"
+    assert test.text == "Test", "Test option text is not 'Test', cannot find text"
+    assert test.is_enabled(), "Test element is not enabled"
+    assert test.is_selected(), "Test element is not selected"
+    assert test.get_attribute("placeholder") == "Test Placeholder", \
+        "Test element placeholder is incorrect"
